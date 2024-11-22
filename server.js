653c7098f55be19ad13d8e722dcd4c67333a2c3f
natarajanmayi <natarajanmayi@gmail.com>
2024-11-22 23:39:21 +0530

    cg

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
